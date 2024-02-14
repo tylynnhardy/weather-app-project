14057c06e04db57c86a082133a82f1f635c4e51e
tylynnhardy <159942806+tylynnhardy@users.noreply.github.com>
2024-02-14 09:31:29 -0800

    added weather icon details

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ function displayTemperature(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />`;
   timeElement.innerHTML = formatDate(date);
   windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
