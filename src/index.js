1ca49937f7bdc0285dc581ec6dc60257751f81ef
tylynnhardy <159942806+tylynnhardy@users.noreply.github.com>
2024-02-13 17:09:59 -0800

    added more details to search engine

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature-value");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "8bc8b03b375bt4da35a05of0462b8403";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
 function citySubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", citySubmit);
+
+searchCity("Anchorage");
